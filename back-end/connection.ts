9bfdd27509821a35f78eba9970adb1a3a726cbb3
Paul Reynolds <paul.reynolds@turnberrysolutions.com>
2024-03-07 13:10:37 -0600

    Initial commit

---
+++ back-end/connection.ts
@@ -0,0 +1,10 @@
+const pgp = require("pg-promise")();
+const db = pgp({
+  host: "localhost",
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+module.exports = db;
