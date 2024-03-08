1b2a85a6d636a47a336b80414f393c2b2e8a4628
Paul Reynolds <paul.reynolds@turnberrysolutions.com>
2024-03-08 09:36:12 -0600

    Initial commit

--- back-end/app.js
+++ back-end/app.js
@@ -9,8 +9,6 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-const { db } = require("./connection.ts");
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -23,8 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-console.log(db);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- back-end/connection.ts
+++ back-end/connection.ts
@@ -7,4 +7,12 @@ const db = pgp({
   password: process.env.POSTGRES_PASSWORD,
 });
 
+db.any('SELECT * FROM accounts')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
 module.exports = db;
