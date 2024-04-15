bd802cc91f2754c669d98f62c64acf2279459c46
Paul Reynolds <paul.reynolds@turnberrysolutions.com>
2024-04-15 09:47:46 -0500

    Cleaned up unused routes in app.js from initialization

--- back-end/app.js
+++ back-end/app.js
@@ -4,8 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 const balancesRouter = require("./balances/balances.router");
 const depositsRouter = require("./deposits/deposits.router");
 const withdrawalsRouter = require("./withdrawals/withdrawals.router");
@@ -22,8 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/balances", balancesRouter);
 app.use("/deposits", depositsRouter);
 app.use("/withdrawals", withdrawalsRouter);
