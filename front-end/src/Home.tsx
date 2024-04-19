6f1c65006fdbe19a58103b9f56ad3f35e7991f0a
Paul Reynolds <paul.reynolds@turnberrysolutions.com>
2024-04-19 09:27:20 -0500

    Cleaning up unused stuff in back-end and making more robust use of TS types on front end, refining refactor for separate components

--- front-end/src/Home.tsx
+++ front-end/src/Home.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
 import { getBalance, makeDeposit, makeWithdrawal } from "./utils/api";
-import React from "react";
-import {Button, FormControl} from '@mui/material';
 import Form from './Form'
 
 type Request = {
@@ -31,19 +28,22 @@ function Home({
   const [request, setRequest] = useState<Request>(initialState);
   const [response, setResponse] = useState<Response>({});
   const [error, setError] = useState<Error>({});
+  const [accountNumber, setAccountNumber] = useState('')
+  const [balance, setBalance] = useState(null)
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: Event) => {
     e.preventDefault();
     e.stopPropagation();
-    setResponse({account_number: null, balance: null})
-    setError({message: null})
+    setResponse({})
+    setError({})
     const ac = new AbortController();
     switch (request.request_type) {
       case 'balanceInquiry':
         await getBalance(request.account_number, ac.signal)
           .then(res => {
-            console.log(`res in handlesubmit: ${JSON.stringify(res)}`)
             setResponse(res)
+            setAccountNumber(res.account_number)
+            setBalance(res.balance)
             console.log(`Response in component's API call: ${JSON.stringify(response)}`)
           }).catch(err => {
             console.log(`error in handlesubmit: ${err}`)
@@ -52,9 +52,7 @@ function Home({
         break;
       case 'deposit':
         await makeDeposit(request.account_number, request.transaction_amount, ac.signal)
-          .then(res => {
-            setResponse(res)
-          }).catch(setError)
+          .then(setResponse).catch(setError)
         break;
       case 'withdrawal':
         await makeWithdrawal(request.account_number, request.transaction_amount, ac.signal)
@@ -70,11 +68,10 @@ function Home({
   return (
     <>
     <h1>Welcome to Advisors Excel ATM!</h1>
-
- 
+    {accountNumber && <h2>Account Number: {accountNumber}</h2>}
       <Form request={request} handleSubmit={handleSubmit} handleChange={changeHandler}/>
     <p>{`${request.account_number}, ${request.transaction_amount}, ${request.request_type}`}</p>
-    <p>{response?.account_number ? `Your account number ${response.account_number} has a new balance of $${response.balance}` : null}</p>
+    <p>{balance ? `Your account number ${accountNumber} has a new balance of $${balance}` : null}</p>
     <div>
       {response?.allBalances && response.allBalances.map((account, i) => (
         <p key={i}>Your account number {account.account_number} has a new balance of ${account.balance}</p>
@@ -83,10 +80,6 @@ function Home({
     <p>{error?.message || 'Lucky you, no errors'}</p>
     </>
   )
-}
-
-Home.propTypes = {
-  initialState: PropTypes.object
 }
 
 export default Home;
