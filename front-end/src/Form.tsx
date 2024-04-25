8db1b1d5494d73dc66808bf457d59e246fa09025
Paul Reynolds <paul.reynolds@turnberrysolutions.com>
2024-04-25 15:05:06 -0500

    Changed request state to form input and did a whooooole bunch of Grid styling

--- front-end/src/Form.tsx
+++ front-end/src/Form.tsx
@@ -1,51 +1,125 @@
 import React from "react";
-import {Button, FormControl, InputLabel, MenuItem, Select, TextField} from '@mui/material';
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 
-function Form ({request, handleSubmit, handleChange}) {
-
+function Form({ formInput, handleSubmit, handleChange }) {
   const transactionOptions = {
-    balanceInquiry: {displayName: 'Balance Inquiry', showAmountField: false},
-    deposit: {displayName: 'Deposit', showAmountField: true},
-    withdrawal: {displayName: 'Withdrawal', showAmountField: true},
-  }
+    balanceInquiry: { displayName: "Balance Inquiry", showAmountField: false },
+    deposit: { displayName: "Deposit", showAmountField: true },
+    withdrawal: { displayName: "Withdrawal", showAmountField: true },
+  };
 
   return (
-    <div className="bg-white">
-      <form onSubmit={handleSubmit}>
-        <FormControl fullWidth>
-          <InputLabel id="request-type-select">Choose your transaction</InputLabel>
-          <Select labelId="request-type-select" id="request_type" label='Choose your transaction' name='request_type' value={request.request_type} onChange={handleChange}>
-          {Object.keys(transactionOptions).map((transactionType, i) => (
-              <MenuItem key={i} value={transactionType}>
-                {transactionOptions[transactionType].displayName}
-              </MenuItem>
-            ))}
-          </Select>
-          <div className="flex justify-between">
-
-
-          <TextField 
-            id="account_number" 
-            name="account_number" 
-            label="Account Number"
-            required 
-            onChange={handleChange}
-            value={request.account_number}
-          />
-          {transactionOptions[request.request_type]?.showAmountField && (
-            <TextField 
-            id="transaction_amount" 
-            name="transaction_amount" 
-            label={`${transactionOptions[request.request_type].displayName} Amount`}
-            value={request.transaction_amount || ''} 
-            onChange={handleChange} />
-          )}
-          </div>
-          <Button type="submit" variant="contained" color="success" disabled={!request.request_type}>Submit</Button>
-        </FormControl>
-      </form>
-    </div>
-  )
+    <Grid
+      container
+      justifyContent="center"
+      spacing={2}
+      margin={2}
+    >
+      <Grid
+        item
+        xs={11}
+        justifyContent="center"
+        className="flex p-3 bg-white"
+      >
+        <form onSubmit={handleSubmit}>
+          <FormControl fullWidth>
+            <Grid
+              container
+              direction="column"
+              spacing={2}
+            >
+              <Grid
+                item
+                xs={12}
+              >
+                <InputLabel id="request-type-select">
+                  Choose your transaction
+                </InputLabel>
+                <Select
+                  fullWidth
+                  labelId="request-type-select"
+                  id="request_type"
+                  label="Choose your transaction"
+                  name="request_type"
+                  value={formInput.request_type}
+                  onChange={handleChange}
+                >
+                  {Object.keys(transactionOptions).map((transactionType, i) => (
+                    <MenuItem
+                      key={i}
+                      value={transactionType}
+                    >
+                      {transactionOptions[transactionType].displayName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+              >
+                <Grid
+                  container
+                  spacing={
+                    formInput.request_type !== "balanceInquiry" &&
+                    formInput.request_type !== ""
+                      ? 2
+                      : 0
+                  }
+                >
+                  <Grid item>
+                    <TextField
+                      id="account_number"
+                      name="account_number"
+                      label="Account Number"
+                      required
+                      onChange={handleChange}
+                      value={formInput.account_number}
+                    />
+                  </Grid>
+                  <Grid item>
+                    {transactionOptions[formInput.request_type]
+                      ?.showAmountField && (
+                      <TextField
+                        id="transaction_amount"
+                        name="transaction_amount"
+                        label={`${
+                          transactionOptions[formInput.request_type].displayName
+                        } Amount`}
+                        value={formInput.transaction_amount || ""}
+                        onChange={handleChange}
+                      />
+                    )}
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid
+                item
+                justifyContent="flex-start"
+              >
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="success"
+                  disabled={!formInput.request_type}
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </FormControl>
+        </form>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Form;
--- front-end/src/Home.tsx
+++ front-end/src/Home.tsx
@@ -1,107 +1,178 @@
 import React, { useState } from "react";
 import { getBalance, makeDeposit, makeWithdrawal } from "./utils/api";
-import Form from './Form'
+import Form from "./Form";
+import { Grid } from "@mui/material";
 
 type Request = {
-  account_number: number | null,
-  transaction_amount: number,
-  request_type: string,
-}
+  account_number: string;
+  transaction_amount: number;
+  request_type: string;
+};
+
+type FormInput = {
+  account_number: string;
+  transaction_amount: number;
+  request_type: string;
+};
 
 type Response = {
-  account_number?: number,
-  balance?: number,
-  allBalances?: Response[],
-  transaction_type?: string,
-  transaction_amount?: number
-}
+  account_number?: number;
+  balance?: number;
+  allBalances?: { account_number: number; balance: number }[];
+  transaction_type?: string;
+  transaction_amount?: number;
+};
 
 type Error = {
-  message?: string
-}
+  message?: string;
+};
 
 function Home({
   initialState = {
-    account_number: null,
+    account_number: "",
     transaction_amount: 0,
-    request_type: ''
+    request_type: "",
   },
 }) {
-  const [request, setRequest] = useState<Request>(initialState);
+  const [formInput, setFormInput] = useState<FormInput>(initialState);
   const [response, setResponse] = useState<Response>({});
   const [error, setError] = useState<Error>({});
-  const [accountNumber, setAccountNumber] = useState<number | null>(null)
-  const [balance, setBalance] = useState<number | null>(null)
-  const [allBalanceInquiry, setAllBalanceInquiry] = useState<boolean>(false);
+  const [accountNumber, setAccountNumber] = useState<number | undefined>(
+    undefined
+  );
+  const [displayBalance, setDisplayBalance] = useState<string | null>(null);
+
+  const formatBalance = (balance: number): string => {
+    return balance.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
 
   const handleSubmit = async (e: Event) => {
     e.preventDefault();
     e.stopPropagation();
-    setResponse({})
-    setError({})
+    setResponse({});
+    setError({});
     const ac = new AbortController();
-    
+
+    const request: Request = {
+      ...formInput,
+      account_number: formInput.account_number,
+    };
+
     const responseSetter = (apiResponse: Response) => {
       setResponse(apiResponse),
-      apiResponse.account_number && setAccountNumber(apiResponse.account_number)
-      setBalance(apiResponse.balance || null)
-      apiResponse.allBalances && setAllBalanceInquiry(true);
-    }
+        setAccountNumber(apiResponse.account_number || undefined);
+      apiResponse.balance &&
+        setDisplayBalance(formatBalance(apiResponse.balance) || null);
+    };
 
     switch (request.request_type) {
-      case 'balanceInquiry':
+      case "balanceInquiry":
         await getBalance(request.account_number, ac.signal)
-        .then(res => {
-          responseSetter(res)
-          console.log(`Response in component's API call: ${JSON.stringify(res)}`)
-          }).catch(err => {
-            console.log(`error in handlesubmit: ${err}`)
-            setError(err)
+          .then((res) => {
+            responseSetter(res);
+            console.log(
+              `Response in component's API call: ${JSON.stringify(res)}`
+            );
           })
-          break;
-          case 'deposit':
-            await makeDeposit(request.account_number, request.transaction_amount, ac.signal)
-            .then(res => {
-            console.log(`Response in component's API call: ${JSON.stringify(res)}`)
-            setResponse(res)
-          }).catch(setError)
-          break;
-          case 'withdrawal':
-            await makeWithdrawal(request.account_number, request.transaction_amount, ac.signal)
-            .then(res => {
-              console.log(`Response in component's API call: ${JSON.stringify(res)}`)
-              setResponse(res)
-            }).catch(setError)
-          }
-    setRequest({...initialState, account_number: accountNumber})
+          .catch((err) => {
+            console.log(`error in handlesubmit: ${err}`);
+            setError(err);
+          });
+        break;
+      case "deposit":
+        await makeDeposit(
+          request.account_number,
+          request.transaction_amount,
+          ac.signal
+        )
+          .then((res) => {
+            console.log(
+              `Response in component's API call: ${JSON.stringify(res)}`
+            );
+            setResponse(res);
+          })
+          .catch(setError);
+        break;
+      case "withdrawal":
+        await makeWithdrawal(
+          request.account_number,
+          request.transaction_amount,
+          ac.signal
+        )
+          .then((res) => {
+            console.log(
+              `Response in component's API call: ${JSON.stringify(res)}`
+            );
+            setResponse(res);
+          })
+          .catch(setError);
+    }
+    setFormInput({
+      ...initialState,
+      account_number: accountNumber?.toString() || "",
+    });
     return () => ac.abort();
-  }
+  };
 
-  const handleChange = ({target: {name, value}}) => {
-    setRequest(prevState => ({...prevState, [name]: value}))
-  }
+  const handleChange = ({ target: { name, value } }) => {
+    setFormInput((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   const displayMessage = {
-    balanceInquiry: `Your account's balance is $${balance}`,
-    deposit: `Your account's balance is ${(balance && balance < 0 && '-')}$${balance} after a deposit of $${response.transaction_amount && response.transaction_amount}`,
-    withdrawal: `Your account's balance is ${(balance && balance < 0 && '-')}$${balance} after a withdrawal of $${response.transaction_amount}`
-  }
+    balanceInquiry: `Your account's balance is ${displayBalance}`,
+    deposit: `Your account's balance is ${displayBalance} after a deposit of $${response.transaction_amount}`,
+    withdrawal: `Your account's balance is ${displayBalance} after a withdrawal of $${response.transaction_amount}`,
+  };
 
   return (
     <>
-    <h1>Welcome to Advisors Excel ATM!</h1>
-    {accountNumber && <h2>Account Number: {accountNumber}</h2>}
-      <Form request={request} handleSubmit={handleSubmit} handleChange={handleChange}/>
-    <p>{response.transaction_type ? displayMessage[response.transaction_type] : null}</p>
-    <div>
-      {allBalanceInquiry && response.allBalances?.map((account, i) => (
-        <p key={i}>Your account number {account.account_number} has a new balance of ${account.balance}</p>
-      ))}
-    </div>
-    <p>{error?.message || null}</p>
+      <Grid
+        container
+        direction="column"
+        spacing={2}
+      >
+        <Grid
+          item
+          justifyContent={"center"}
+        >
+          <h1>Welcome to Advisors Excel ATM!</h1>
+          <Grid item>
+            {accountNumber && <h2>Account Number: {accountNumber}</h2>}
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          justifyContent={"center"}
+        >
+          <Form
+            formInput={formInput}
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+          />
+        </Grid>
+        <Grid item>
+          <p>
+            {response.transaction_type
+              ? displayMessage[response.transaction_type]
+              : null}
+          </p>
+          <div>
+            {response.allBalances &&
+              response.allBalances?.map((account, i) => (
+                <p key={i}>
+                  Your account number {account.account_number} has a new balance
+                  of {formatBalance(account.balance)}
+                </p>
+              ))}
+          </div>
+          <p>{error?.message || null}</p>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
 
 export default Home;
-
